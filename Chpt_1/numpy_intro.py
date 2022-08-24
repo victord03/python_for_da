d2f06b8b1d0ebf5e7ddaf961eccbb16c70a439b8
victord03 <vic.kaklamanis@gmail.com>
2022-08-24 22:26:24 +0300

    Added a few notes.

---
+++ Chpt_1/numpy_intro.py
@@ -0,0 +1,6 @@
+from numpy.random import randn
+import pylint
+
+data = {i: randn() for i in range(7)}
+
+print(data)
---
+++ General/notation.py
@@ -0,0 +1,47 @@
+"""
+Italic
+Indicates new terms, URLs, email addresses, filenames, and file extensions.
+
+Constant width
+Used for program listings, as well as within paragraphs to refer to program ele‐
+ments such as variable or function names, databases, data types, environment
+variables, statements, and keywords.
+
+Constant width bold
+Shows commands or other text that should be typed literally by the user.
+
+Constant width italic
+Shows text that should be replaced with user-supplied values or by values deter‐
+mined by context.
+
+Squirrel
+This element signifies a tip or suggestion.
+
+Crow
+This element signifies a general note.
+
+Scorpion
+This element indicates a warning or caution.
+
+Book repo: http://github.com/wesm/pydata-book (The book is 2nd edition. Choose corresponding branch.)
+"""
+
+"""
+Interacting with the outside world
+Reading and writing with a variety of file formats and data stores
+
+Preparation
+Cleaning, munging, combining, normalizing, reshaping, slicing and dicing, and
+transforming data for analysis
+
+Transformation
+Applying mathematical and statistical operations to groups of datasets to derive
+new datasets (e.g., aggregating a large table by group variables)
+
+Modeling and computation
+Connecting your data to statistical models, machine learning algorithms, or other
+computational tools
+
+Presentation
+Creating interactive or static graphical visualizations or textual summaries
+"""
