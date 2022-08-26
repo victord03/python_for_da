421359c31a98b1994e7cb1cc36d64ff94d0b44da
victord03 <vic.kaklamanis@gmail.com>
2022-08-26 17:10:49 +0300

    Added pandas intro.

---
+++ Pandas/Chpt_1/pandas_intro.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+DECO = "-" * 50
+NL = "\n"
+
+my_dictionary_of_ds_weapons = {
+    "name": ["Short Sword", "Mace", "Farron Ultra Greatsword"],
+    "damage": [2, 5, 0],
+    "class": ["Sword", "Mace", "Ultra Greatsword"],
+    "is_unique": [False, False, True],
+    "upgrade_level": [0, 1, 1]
+}
+
+data = pd.DataFrame(my_dictionary_of_ds_weapons)
+
+"""
+DataFrame
+
+    attributes
+        columns
+        
+        
+    methods
+        head(x: int or None)
+        tail(x: int or None)
+        info()
+        
+
+
+access rows as if calling the index of a dict
+    DataFrame['row_name']
+        
+access columns via      
+"""
+
+print(f"{NL}Data.info(){NL}")
+print(data.info())
+print(DECO + NL)
+print(f"Data['class']{NL}")
+print(data["class"])
+print(DECO + NL)
+print(f"{NL}Data.columns{NL}")
+print(data.columns)
+print(DECO + NL)
+print(f"{NL}Data{NL}")
+print(data)
