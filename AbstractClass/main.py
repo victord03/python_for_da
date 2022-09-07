6d47c457e3bc472a335dc4dd7a44feab421412eb
victord03 <vic.kaklamanis@gmail.com>
2022-09-07 11:09:08 +0300

    Added a few studying files. AbstractClass, DataClass, Enum.

--- AbstractClass/main.py
+++ AbstractClass/main.py
@@ -1,4 +1,7 @@
-from abc import ABC, abstractmethod as absmtd
+import random
+from abc import ABC, abstractmethod
+
+from random import randint,triangular
 
 
 class Employee(ABC):
@@ -10,7 +13,7 @@ class Employee(ABC):
         self.name = name
         self.last_name = last_name
 
-    @absmtd
+    @abstractmethod
     def create_email(self):
         ...
 
@@ -31,9 +34,68 @@ class Dev(Employee):
         return name + last_name + email
 
 
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def perimeter(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+
+    length: int
+    width: int
+
+    def __init__(self, length: int, width: int) -> None:
+        self.length = length
+        self.width = width
+
+    def area(self) -> int:
+        return self.length * self.width
+
+    def perimeter(self) -> int:
+        return 2 * (self.length + self.width)
+
+
+def triangular_distribution(lower_bound: float, upper_bound: float) -> float:
+    return round(triangular(lower_bound, upper_bound), 2)
+
+
 def main():
+
+    # ABSTRACT CLASSES AND METHODS
     employee_1 = Dev("Victor", "Kaklamanis")
     print(employee_1.display_name())
+
+    # REFACTOR EXERCISE (COMPLETED)
+    # this_rectangle = Rectangle(length=3, width=7)
+    # print(this_rectangle.area())
+    # print(this_rectangle.perimeter())
+
+    # DUPLICATES EXERCISE (COMPLETED)
+    # my_list = [randint(1, 25) for _ in range(4000)]
+    # my_list.extend([2, 2])
+
+    # print(my_list)
+    # print()
+
+    # duplicates = list(set(x for x in my_list if my_list.count(x) > 1))
+
+    # print(duplicates)
+    # print()
+
+    # frequency_dict = {x: my_list.count(x) for x in sorted(my_list)}
+    # print(frequency_dict)
+
+    # MAP
+    # upper_bounds = [randint(1, 20) for _ in range(20)]
+    # lower_bounds = [randint(19, 40) for _ in range(20)]
+    # results = list(map(triangular_distribution, upper_bounds, lower_bounds))
+    # print(results)
 
 
 if __name__ == "__main__":
