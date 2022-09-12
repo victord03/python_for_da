c35b970d88b83488124940d12175e847e99fffb7
victord03 <vic.kaklamanis@gmail.com>
2022-09-12 23:52:05 +0300

    Page 76.

--- General/general_main.py
+++ General/general_main.py
@@ -11,6 +11,12 @@ def main():
     a, b = zip(*squares)
     # print(a, b)
 
+    # DICTS
+    my_dict_1 = {"a": 1, "b": 2, "c": 3, "d": 7}
+    my_dict_1.update({"d": 4, "e": 5})  # values to already existing keys are replaced by the new values
+    del my_dict_1["e"]  # can be used instead of .pop() if the value will not be used
+    # print(my_dict_1)
+
 
 if __name__ == "__main__":
     main()
