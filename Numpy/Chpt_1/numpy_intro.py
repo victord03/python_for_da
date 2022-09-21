3fd4635c2b5b015432ac1cef67178196e6a3613f
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2022-09-22 01:53:16 +0300

    Up to page 126.

--- Numpy/Chpt_1/numpy_intro.py
+++ Numpy/Chpt_1/numpy_intro.py
@@ -1,4 +1,6 @@
 # import matplotlib.pyplot as plt
+import random
+
 from numpy.random import randn
 from datetime import datetime as dt
 import numpy as np
@@ -207,6 +209,14 @@ new_ndarray = np.ones((3, 4, 2), dtype=np.int8)
 
 VALUABLE CHEAT SHEET (INDEXING, SLICING)
 
+3 x 1 x 4
+
+[
+    [  { [0] }, { [1] }, { [2] }, { [3] }  ],     < index [0]
+    [  { [0] }, { [1] }, { [2] }, { [3] }  ],     < index [1]
+    [  { [0] }, { [1] }, { [2] }, { [3] }  ],     < index [2]
+]
+
 2 x 2 "Two containers, each one of those containers containing two elements inside."
 
 [
@@ -293,8 +303,6 @@ It is also possible to mix and match boolean arrays with slices or integers. Exa
 names = np.array(["Bob", "Alfred", "Bob", "Joyce", "Will", "Joyce", "Joe"])
 random_data_2 = np.random.randn(7, 4)
 
-print(random_data_2)
-print("\n\n")
 boolean_array_bob = names == "Bob"
 selection = random_data_2[boolean_array_bob]  # excuse me. wat.
 
@@ -308,3 +316,293 @@ boolean_array_bob_or_joyce = (names == "Bob") | (names == "Joyce")
 list_of_indices = [index for index, boolean in enumerate(list(boolean_array_bob_or_joyce)) if boolean]
 selection_bob_or_joyce = random_data_2[boolean_array_bob_or_joyce]
 selection_not_bob_nor_joyce = random_data_2[~boolean_array_bob_or_joyce]
+
+# FANCY INDEXING (page 115)
+arr3 = np.empty((8, 4))
+
+for i in range(8):
+    arr3[i] = i
+
+# print(arr3)
+# print("\n\n")
+
+# Passing a list of indices as a index, will return the corresponding containers at the specified indices
+# Handles negative indices as in standard Python.
+specific_rows = arr3[[0, 3, 5, 7]]
+
+arr4 = np.arange(32).reshape((8, 4))  # reshape will be explored later in the book.
+
+# Passing a tuple of lists of indices returns, for each container at index on the first list, the element at index
+# specified in the second list.
+selection_4 = arr4[[0, 2, 5], [1, 2, 0]]  # This returns the positions (0, 1), (2, 2) and (5, 0) from the ndarray
+
+selection_5 = arr4[[0, 2, 5]]
+selection_6 = arr4[[0, 2, 5]][:, [3, 2]]  # This returns the rows [0, 2, 5] and then the columns 3 and 2
+
+# TRANSPOSING ARRAYS AND SWAPPING AXES (page 116)
+arr5 = np.arange(15).reshape((5, 3))
+
+# the special .T attribute flips the array horizontally or vertically, maintaining the order on which the elements
+# appeared in. See the quote block for more info on the ordering.
+
+"""
+
+3 x 5 shape(3, 5)
+
+OVERVIEW
+
+    Initial state (arr):
+        [0  1  2  3  4]
+        [5  6  7  8  9]
+        [10 11 12 13 14]
+    
+    Transposed state (arr.T): 
+        [0  5 10]
+        [1  6 11]
+        [2  7 12]
+        [3  8 13]
+        [4  9 14]
+
+INDICES ANALYSIS
+
+    Initial state (arr):
+        [  [0][0]  [0][1]  [0][2]  [0][3]  [0][4]  ]
+        [  [1][0]  [1][1]  [1][2]  [1][3]  [1][4]  ]
+        [  [1][0]  [1][1]  [1][2]  [1][3]  [1][4]  ]
+    
+    Transposed state (arr.T): 
+        [  [0][0]  [1][0]  [2][0]  ]
+        [  [0][1]  [1][1]  [2][1]  ]
+        [  [0][2]  [1][2]  [2][2]  ]
+        [  [0][3]  [1][3]  [2][3]  ]
+        [  [0][4]  [1][4]  [2][4]  ]
+
+
+5 x 3 shape(5, 3)
+
+OVERVIEW
+
+    Initial state (arr):
+        [ 0  1  2]
+        [ 3  4  5]
+        [ 6  7  8]
+        [ 9 10 11]
+        [12 13 14]
+
+    Transposed state (arr.T): 
+        [ 0  3  6  9 12]
+        [ 1  4  7 10 13]
+        [ 2  5  8 11 14]
+
+INDICES ANALYSIS
+
+    Initial state (arr):
+        [  [0][0]  [0][1]  [0][2]  ]
+        [  [1][0]  [1][1]  [1][2]  ]
+        [  [2][0]  [2][1]  [2][2]  ]
+        [  [3][0]  [3][1]  [3][2]  ]
+        [  [4][0]  [4][1]  [4][2]  ]
+        
+    
+    Transposed state (arr.T): 
+        [  [0][0]  [1][0]  [2][0]  [3][0]  [4][0]  ]
+        [  [0][1]  [1][1]  [2][1]  [3][1]  [4][1]  ]
+        [  [0][2]  [1][2]  [2][2]  [3][2]  [4][2]  ]
+"""
+
+# UNIVERSAL FUNCTIONS: FAST ELEMENT-WISE ARRAY FUNCTIONS (page 118)
+
+"""
+
+np.minimum(), np.maximum()
+
+arr6 = np.random.randn(4, 4)
+arr7 = np.random.randn(4, 4)
+
+print("\nArray 1")
+print(abs(arr6))
+print("\n\nArray 2")
+print(abs(arr7))
+
+print("\n\nMaximum")
+print(np.maximum(abs(arr6), abs(arr7)))
+print("\n\nMinimum")
+print(np.minimum(abs(arr6), abs(arr7)))
+
+Unary ufuncs (page 120)
+
+Binary universal funcs (page 120)
+
+.add                    Add corresponding elements in arrays
+.subtract               Subtract elements in second array from first array
+.multiply               Multiply array elements
+.divide, floor_divide   Divide or oor divide (truncating the remainder)
+.power                  Raise elements in first array to powers indicated in second array
+.maximum, fmax          Element-wise maximum; fmax ignores NaN
+.minimum, fmin          Element-wise minimum; fmin ignores NaN
+.mod                    Element-wise modulus (remainder of division)
+.copysign               Copy sign of values in second argument to values in first argument
+
+.greater
+.greater_equal
+.less
+.less_equal
+equal
+.not_equal              Perform element-wise comparison, yielding boolean array (equivalent to infix operators >, >=, 
+<, <=, ==, !=)
+
+.logical_and
+.logical_or
+.logical_xor            Compute element-wise truth value of logical operation (equivalent to infix operators & |, ^)
+
+"""
+
+# ARRAY-ORIENTED PROGRAMMING WITH ARRAYS (page 121)
+# The np.meshgrid function takes two 1D arrays and produces two 2D matrices corresponding
+# to all pairs of (x, y) in the two arrays
+
+# EXPRESSING CONDITIONAL LOGIC AS ARRAY OPERATIONS (page 122)
+
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5], dtype=np.float64)
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5], dtype=np.float64)
+cond = np.array([True, False, True, True, False], dtype=np.bool_)
+
+result = np.where(cond, xarr, yarr)
+
+"""
+Suppose you had a matrix of randomly generated data and you wanted to replace 
+some of the values, based on a specific condition, using the boolean indexing.
+"""
+arr6 = np.random.randn(4, 4)
+cond_arr = (-1.2 < arr6) & (arr6 < 1.2)  # Df = (-1.2, 1.2)
+result_cond = np.where(cond_arr, 1, 0)  # Replace all values between -1.2 and 1.2 with 1, and the rest with 0.
+
+neg_to_zero = arr6 < 0
+"\nNegatives to zero"
+np.where(neg_to_zero, 0, arr6)  # Replace all negative values with 0, leave the others intact.
+
+
+# MATHEMATICAL AND STATISTICAL METHODS (page 124)
+arr7 = np.array([np.arange(1, 17)], np.float16)
+arr7 = arr7.reshape(4, 4)
+
+# Populating with semi-random data manually (np.random.randn() could be used but slicing is practiced here).
+arr7[[0, 0, 0, 2, 3], [0, 1, 2, 0, 2]] = -2.008
+arr7[[0, 1, 1], [3, 2, 3]] = 2.48
+arr7[[1, 2, 2, 2], [1, 0, 2, 3]] = -.966
+arr7[[1, 2, 3, 3, 3], [0, 1, 0, 1, 3]] = 1.114
+
+print_mean = "\nMean (Overall)"
+# Mean, sum, std (standard deviation). Can be called as methods or top-level NumPy functions.
+mean_array_overall = arr7.mean()  # optional argument axis=0 for the columns (vertical) or axis=1 for the rows (horizontal).
+assert arr7.mean() == np.mean(arr7)
+# print("_" * 36)
+
+print_mean_1 = "\nMean (Horizontal)"
+mean_array_hori = arr7.mean(axis=1)
+
+"""
+Could be accessed as a list
+
+print("\nMean (row 0)")
+print(arr7.mean(axis=1)[0])
+print("\nMean (row 1)")
+print(arr7.mean(axis=1)[1])
+print("\nMean (row 2)")
+print(arr7.mean(axis=1)[2])
+print("\nMean (row 3)")
+print(arr7.mean(axis=1)[3])"""
+
+print_mean_2 = "\nMean (Vertical)"
+mean_array_vert = arr7.mean(axis=0)
+
+"""
+Could be accessed as a list
+
+print("\n\nMean (column 0)")
+print(arr7.mean(axis=0)[0])
+print("\nMean (column 1)")
+print(arr7.mean(axis=0)[1])
+print("\nMean (column 2)")
+print(arr7.mean(axis=0)[2])
+print("\nMean (column 3)")
+print(arr7.mean(axis=0)[3])"""
+
+print_sum = "\nSum (Overall)"
+sum_array_overall = arr7.sum()
+assert arr7.sum() == np.sum(arr7)
+# print("_" * 36)
+
+print_sum_1 = "\nSum (Horizontal)"
+sum_array_hori = arr7.sum(axis=1)
+
+print_sum_2 = "\nSum (Vertical)"
+sum_array_vert = arr7.sum(axis=0)
+
+# Cumulative sum (cumsum) returns an array with the cumulative sum for each number encountered so far,
+# at each index.
+arr8 = np.array([x for x in range(1, 9)])
+cumulative_sum = arr8.cumsum()  # cumulative sum (up to each index)
+
+# Cumulative product (cumprod) returns an array with the cumulative product.
+arr9 = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+cumulative_product = np.cumprod(arr9, axis=1)
+
+"""print("\nArray")
+print(arr7)
+print("\nMean:")
+print(arr7.mean())
+print("\nStandard deviation:")
+print(arr7.std())
+print("\nVariance:")
+print(arr7.var())"""
+
+# Basic array statistical methods (page 125)
+people_data = {
+    "Rose": {
+        "age": 18, "sex": True, "income": 650, "children": 0, "pets": 0
+    },
+    "Victor": {
+        "age": 31, "sex": False, "income": 1_150, "children": 0, "pets": 0
+    },
+    "Spyros": {
+        "age": 58, "sex": False, "income": 1_200, "children": 1, "pets": 2
+    },
+    "Dimitra": {
+        "age": 50, "sex": True, "income": 450, "children": 3, "pets": 0
+    },
+    "Billy": {
+        "age": 56, "sex": False, "income": 3_220, "children": 2, "pets": 4
+    },
+    "Marie": {
+        "age": 45, "sex": True, "income": 1_950, "children": 1, "pets": 1
+    },
+}
+
+incomes = np.array(
+    [inner_dict[category]
+     for name, inner_dict in people_data.items()
+     for category in inner_dict if category == "income"]
+)
+
+age_values = np.array(
+    [inner_dict[category]
+     for name, inner_dict in people_data.items()
+     for category in inner_dict if category == "age"]
+)
+
+age_over_income = np.array(list(zip(age_values, incomes))).reshape(len(age_values), 2)
+
+"""print("\nIncomes (sorted):")
+print(sorted(incomes))
+print("\nMean:")
+print(round(incomes.mean(), 2))
+print("\nStandard deviation:")
+print(round(incomes.std()))"""
+
+print("\nAge and income:")
+print(age_over_income)
+print("\nMean (age):")
+print(round(np.float16(age_over_income[:, [0]].mean()), 1))
+print("\nMean (income):")
+print(round(np.float16(age_over_income[:, [1]].mean()), 1))
